68ba6e84e16fb0531eb0f68c972a6c132b05e48e
uriid1 <70856305+uriid1@users.noreply.github.com>
2022-05-17 02:40:45 +0300

    Update game.sh

--- game.sh
+++ game.sh
@@ -1,3 +1,17 @@
-clear
-gcc game.c -o AppleWar -Wno-unused -lncurses -Wall
+####--------------------------------####
+#--# Author:   by uriid1            #--#
+#--# License:  GNU GPLv3            #--#
+#--# Telegram: @main_moderator      #--#
+#--# E-mail:   appdurov@gmail.com   #--#
+####--------------------------------####
+
+# Del old
+if [ -f AppleWar ]; then
+	rm AppleWar
+fi
+
+# Build new
+gcc game.c -o AppleWar -Wno-unused -lncurses
+
+# Run
 ./AppleWar
